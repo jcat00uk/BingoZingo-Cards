74d95a4d1a3b947a34273626402ed51b6570031b
jcat00uk <github@thecrazycat.com>
2026-04-01 20:15:49 +0100

    Web build 20260401-2015

--- changelog.js
+++ changelog.js
@@ -1,6 +1,6 @@
 window.CHANGELOG = {
 
-  '20260401-2012': {
+  '20260401-2015': {
     changes: [
       { tag: 'new', text: 'Add inmage to bingocaller start infograpic', editions: 'all' },
       { tag: 'new', text: 'Changelog popup shows on version update with What\'s New entries per edition', editions: 'all' },
