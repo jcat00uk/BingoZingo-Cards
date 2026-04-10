1e5a2e88c49875896788c926f66dbe3ce248a4f0
jcat00uk <github@thecrazycat.com>
2026-04-10 19:27:19 +0100

    Web build 20260410-192717

--- changelog.js
+++ changelog.js
@@ -1,6 +1,6 @@
 window.CHANGELOG = {
 
-  '20260410-190659': {
+  '20260410-192717': {
     changes: [
       { tag: 'fix', text: 'Fixed some page scaling issues', editions: 'full' },
       { tag: 'new', text: 'Added ads', editions: 'all' },
