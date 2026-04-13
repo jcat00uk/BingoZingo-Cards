afbb959e32741cebdd3e5d250e754616d84eef2a
jcat00uk <github@thecrazycat.com>
2026-04-13 21:48:16 +0100

    Web build 20260413-214812

--- game/changelog.js
+++ game/changelog.js
@@ -1,6 +1,6 @@
 window.CHANGELOG = {
 
-  '20260413-213933': {
+  '20260413-214812': {
     changes: [
       { tag: 'fix', text: 'Fixed some page scaling issues', editions: 'full' },
       { tag: 'new', text: 'Added ads', editions: 'all' },
